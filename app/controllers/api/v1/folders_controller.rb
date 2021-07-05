64d3e62267e001662878a713fa701dbcec8cad2c
Rhelli <rory.hellier@googlemail.com>
2021-07-05 14:35:02 +0100

    Continuation: Updates Folders and Topics Controllers:
     - Adds index and show actions for both Folder and Topics controllers'

--- app/controllers/api/v1/folders_controller.rb
+++ app/controllers/api/v1/folders_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::FoldersController < ApplicationController
   def index
+    @folders = Folders.all
+    if @folders
+      render json: @folders, each_serializer: FolderSerializer
+    else
+      render json: { status: 500, errors: ['No Folders Found.'] }
+    end
   end
 
   def show
+    @folder = Folder.find(folder_params[:id])
+    if @folder
+      render json: { folder: @folder }
+    else
+      render json: { status: 500, errors: ['Folder Does Not Exist.'] }
+    end
+  end
+
+  def folder_params
+    params.require(:folder).permit(:id, :title, :description, :started)
   end
 end
--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::TopicsController < ApplicationController
   def index
+    @topics = Topic.all
+    if @topic
+      render json: @topics, each_serializer: TopicSerializer
+    else
+      render json: { status: 500, errors: ['No Topics Found'] }
+    end
   end
 
   def show
+    @topic = Topic.find(topic_params[:id])
+    if @topic
+      render json: { topic: @topic }
+    else
+      render json: { status: 500, errors: ['Topic Does Not Exist'] }
+    end
   end
 end
