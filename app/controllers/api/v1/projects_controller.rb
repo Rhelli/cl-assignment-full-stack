e6d75a161a26817b8f14d4d5ba1ff61df8cda3e5
Rhelli <rory.hellier@googlemail.com>
2021-07-05 13:48:19 +0100

    Adds Basic Index and Show Functions To The Properties Controller

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,9 +2,21 @@ class Api::V1::ProjectsController < ApplicationController
   def index
     @projects = Project.all
     if @projects
-      render json: @
+      render json: @projects, each_serializer: ProjectSerializer
+    else
+      render json: { status: 500, errors: ['No Projects Found.'] }
+    end
   end
 
   def show
+    @project = Project.find(project_params[:id])
+    if @project
+      render json: { project: @project }
+    else
+      render json: { status: 500, errors: ['Project Does Not Exist.']}
+  end
+
+  def project_params
+    params.require(:project).permit(:id, :title, :description, :started, :cost, :folders_id)
   end
 end
